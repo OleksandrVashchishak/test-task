0501569d9f6a90525d8826c6a49ff24e68a78c05
OleksandrVashchishak <67052863+OleksandrVashchishak@users.noreply.github.com>
2021-12-20 16:40:10 +0200

    add

--- js/main.js
+++ js/main.js
@@ -3,9 +3,7 @@
 if (document.querySelector('.portfolio__item')) {
 
     const historyTitle = 'project'
-    const historyState = { foo: "bar" }
-
-
+    const historyState = { page: "project" }
 
     const projectUrl = window.location.href.split('?')
     if (projectUrl[projectUrl.length - 1] && projectUrl[projectUrl.length - 1] == 'american-hospital-tbilsi') {
